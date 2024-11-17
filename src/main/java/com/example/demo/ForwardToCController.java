c4b5d659772f3efb7a54f98cb1ccb659fe5641fa
penggong <1079747870@qq.com>
2024-11-17 12:43:13 +0800

    增加restful接口

--- src/main/java/com/example/demo/ForwardToCController.java
+++ src/main/java/com/example/demo/ForwardToCController.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -35,4 +36,22 @@ public class ForwardToCController {
         return resEntity.getBody();
 	}
 	
+        @RequestMapping("/forwardTo/{targetType}/{targetApi}")
+	public String forwardTo(@PathVariable String targetType, @PathVariable String targetApi) {
+		ResponseEntity<String> resEntity = null;
+		String url = "";
+		if(targetType.equals("sleep")){
+		    url = "http://details3:8080/"+targetType+"?time=" +targetApi;
+		}
+		if(targetType.equals("createObjectLimitCount")){
+                    url = "http://details3:8080/"+targetType+"?time=" +targetApi;
+                }
+		if(targetType.equals("oom")){
+                    url = "http://details3:8080/"+targetType+"?count=" +targetApi;
+                }
+		System.out.println("url:"+url);
+		resEntity = restTemplate.getForEntity(url, String.class);
+        //resEntity = restTemplate.postForEntity(url, "", String.class);
+        return resEntity.getBody();
+	}
 }
